7e06d3e707dec41de760c4e6ef756f03a433d9a5
g0mb4 <gomba007@gmail.com>
2021-05-31 13:18:36 +0200

    test program fixed

--- include/Display.h
+++ include/Display.h
@@ -14,7 +14,7 @@ public:
 
     uint16_t read(uint32_t address) const override { return 0; }
     void write(uint32_t, uint16_t value) override {
-        m_message += (char)(value >> 8);
+        m_message += (char)(value);
     }
 
     const std::string & message() const { return m_message; }
