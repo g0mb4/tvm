cb7e926e736445ff166a852703057d3df4a4410f
g0mb4 <gomba007@gmail.com>
2021-06-03 11:43:26 +0200

    CPUInstructions

---
+++ src/CPUInstructions.cpp
@@ -0,0 +1,152 @@
+#include "CPU.h"
+
+void CPU::mov(){
+    uint16_t source, destination, address, reg;
+
+    switch (m_current_instruction->source_addressing()) {
+    case Instruction::AddressingMode::Instant:
+        source = m_current_instruction->additional_word_source();
+        break;
+    case Instruction::AddressingMode::Direct:
+        address = m_current_instruction->additional_word_source();
+        source = m_bus->read(address);
+        break;
+    default:
+        m_error_string = "Unimplemented 'mov' source addressing: " + m_current_instruction->source_addressing_string();
+        return;
+    };
+
+    switch (m_current_instruction->destination_addressing()) {
+    case Instruction::AddressingMode::DirectRegister:
+        destination = m_current_instruction->destination_register();
+        m_registers[destination] = source;
+        break;
+    default:
+            m_error_string = "Unimplemented 'mov' destination addressing: " + m_current_instruction->destination_addressing_string();
+        return;
+    };
+}
+
+void CPU::lea(){
+    uint16_t source, destination;
+
+    switch (m_current_instruction->source_addressing()) {
+    case Instruction::AddressingMode::Direct:
+        source = m_current_instruction->additional_word_source();
+        break;
+    default:
+        m_error_string = "Unimplemented 'lea' source addressing: " + m_current_instruction->source_addressing_string();
+        return;
+    };
+
+    switch (m_current_instruction->destination_addressing()) {
+    case Instruction::AddressingMode::DirectRegister:
+        destination = m_current_instruction->destination_register();
+        m_registers[destination] = source;
+        break;
+    default:
+        m_error_string = "Unimplemented 'lea' destination addressing: " + m_current_instruction->destination_addressing_string();
+        return;
+    };
+}
+
+void CPU::jnz(){
+    if(m_zero_flag){
+        return;
+    }
+
+    uint16_t destination;
+
+    switch (m_current_instruction->destination_addressing()) {
+    case Instruction::AddressingMode::Direct:
+        destination = m_current_instruction->additional_word_destination();
+        m_program_counter = destination;
+        break;
+    default:
+        m_error_string = "Unimplemented 'jnz' destination addressing: " + m_current_instruction->destination_addressing_string();
+        return;
+    };
+}
+
+void CPU::hlt(){
+    m_halted = true;
+}
+
+void CPU::prn(){
+    uint16_t destination, address, reg;
+
+    switch (m_current_instruction->destination_addressing()) {
+    case Instruction::AddressingMode::IndirectRegister:
+        reg = m_current_instruction->destination_register();
+        address = m_registers[reg];
+        destination = m_bus->read(address);
+        m_bus->write(Display::address, destination);
+        break;
+    default:
+        m_error_string = "Unimplemented 'mov' destination addressing: " + m_current_instruction->destination_addressing_string();
+        return;
+    }
+}
+
+void CPU::sub() {
+        uint16_t source, destination, address, reg;
+
+        switch (m_current_instruction->source_addressing()) {
+        case Instruction::AddressingMode::Instant:
+                source = m_current_instruction->additional_word_source();
+                break;
+        case Instruction::AddressingMode::Direct:
+                address = m_current_instruction->additional_word_source();
+                source = m_bus->read(address);
+                break;
+        default:
+                m_error_string = "Unimplemented 'sub' source addressing: " + m_current_instruction->source_addressing_string();
+                return;
+        };
+
+        switch (m_current_instruction->destination_addressing()) {
+        case Instruction::AddressingMode::DirectRegister:
+                destination = m_current_instruction->destination_register();
+
+                m_carry_flag = source > m_registers[destination];
+                m_registers[destination] -= source;
+                m_zero_flag = m_registers[destination] == 0;
+
+                break;
+        default:
+                m_error_string = "Unimplemented 'sub' destination addressing: " + m_current_instruction->destination_addressing_string();
+                return;
+        };
+}
+
+void CPU::inc() {
+        uint16_t destination;
+
+        switch (m_current_instruction->destination_addressing()) {
+        case Instruction::AddressingMode::DirectRegister:
+                destination = m_current_instruction->destination_register();
+                m_registers[destination] += 1;
+                break;
+        default:
+                m_error_string = "Unimplemented 'inc' destination addressing: " + m_current_instruction->destination_addressing_string();
+                return;
+        };
+}
+
+void CPU::jnc() {
+        if (m_carry_flag) {
+                return;
+        }
+
+        uint16_t destination;
+
+        switch (m_current_instruction->destination_addressing()) {
+        case Instruction::AddressingMode::Direct:
+                destination = m_current_instruction->additional_word_destination();
+                m_program_counter = destination;
+                break;
+        default:
+                m_error_string = "Unimplemented 'jnc' destination addressing: " + m_current_instruction->destination_addressing_string();
+                return;
+        };
+}
